d07375ff067542f208e174a6bc354264dc99583a
Eloise Gasper <eloisegasper95+7@gmail.com>
2024-06-20 14:17:46 +0800

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import { Box, GlobalStyles } from '@bigcommerce/big-design';
+import Header from '../components/header';
+import type { AppProps } from 'next/app';
+import SessionProvider from '../context/session';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+    <>
+        <GlobalStyles />
+
+        <Box marginHorizontal="xxxLarge" marginVertical="xxLarge">
+            <Header />
+            <SessionProvider>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </Box>
+
+    </>
+);
+
+export default MyApp;
